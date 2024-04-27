9fe4e31aee475c4a8676a9dbd1370ef94fe0a555
MalkapuramRishikesh <168266988+MalkapuramRishikesh@users.noreply.github.com>
2024-04-27 15:36:11 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -35,6 +35,7 @@ async function checkWeather(city){
          document.querySelector(".weather").style.display = "block";
          document.querySelector(".error").style.display = "none";
      }
+    // hi
 
     }
 
